fa580b237370a466682eb5954146b2ba71788007
stern15 <ssemasuka@gmail.com>
2016-03-12 12:57:36 +0300

    adding the background picture and and dependency for the card view

--- app/src/main/java/com/semasuka/converter/MainActivity.java
+++ app/src/main/java/com/semasuka/converter/MainActivity.java
@@ -2,21 +2,11 @@ package com.semasuka.converter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
 
-    public void clickMe(View view){
-
-        EditText myTextField = (EditText) findViewById(R.id.textField);
-
-        Log.i("textField value",myTextField.getText().toString());
-
-
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
