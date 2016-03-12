7088e63995750bb5ff753b21d3c8a1581e4eab8c
stern15 <ssemasuka@gmail.com>
2016-03-13 01:07:38 +0300

    final commit

--- app/src/main/java/com/semasuka/converter/MainActivity.java
+++ app/src/main/java/com/semasuka/converter/MainActivity.java
@@ -3,11 +3,24 @@ package com.semasuka.converter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
     public void convert(View view){
+
+        EditText myAmount= (EditText) findViewById(R.id.dollarField);
+
+
+        Double Dollar=Double.parseDouble(myAmount.getText().toString());
+
+        Double Kes= Dollar * 101;
+
+
+        Toast.makeText(getApplicationContext(),"KES "+ Kes.toString(),Toast.LENGTH_SHORT).show();
+
 
 
     }
