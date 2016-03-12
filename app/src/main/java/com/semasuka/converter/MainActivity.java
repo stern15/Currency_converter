c8fe72957fe615e7b0161a0fc5297a855c2e7267
stern15 <ssemasuka@gmail.com>
2016-03-12 21:13:49 +0300

    adding an icon to the app

--- app/src/main/java/com/semasuka/converter/MainActivity.java
+++ app/src/main/java/com/semasuka/converter/MainActivity.java
@@ -2,9 +2,15 @@ package com.semasuka.converter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    public void convert(View view){
+
+
+    }
 
 
     @Override
